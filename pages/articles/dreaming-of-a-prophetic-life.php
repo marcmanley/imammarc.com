232c46321f3454f5d9afa197c96d6e4aea0a1cac
Marc Manley <marcmanleydev@gmail.com>
2023-07-22 12:47:02 -0700

    renamed template files

---
+++ pages/articles/dreaming-of-a-prophetic-life.php
@@ -0,0 +1,98 @@
+<?php include('../../header-main.php'); ?>
+
+<?php include('../../meta-1.php'); ?>
+
+<?php // begin fit-at-50.php ?>
+
+<?php // Social Media Thumbnail ?> 
+<meta property="og:title" content="Imam Marc Manley">
+	<meta property="og:description" content="Imam Marc Manley's Official Website">
+	<meta property="og:image" content="https://imammarc.com/media/imgs/fit-at-50-one-year-update.png">
+	<meta property="og:url" content="https://imammarc.com/pages/non-articles/fit-at-50.php"><!-- update thumbnail -->
+	<meta name="twitter:card" content="summary_large_image">
+  <meta property="og:type" content="website">
+
+  <?php include('../../meta-2.php'); ?>
+
+  <title>Imam Marc &mdash; Title Goes Here</title>
+
+  <style>
+  .pages {
+    margin-top: 15px;
+  }
+
+  .home { 
+    margin-top: 15px;
+  }
+
+  .home a, .home a:link, .home a:visited {
+	text-decoration: none;
+	color: #fff;
+	background-color: #0455BF;
+	font-weight: 600;
+	padding: 5px;
+  margin-top: 10px;
+  text-transform: uppercase;
+  font-family: 'Days One', sans-serif;
+  border-radius: 5px;
+  font-size: 1rem;
+}
+
+  .date {
+    color: #fff;
+    background-color: #000;
+    padding: 5px 10px 0px 10px;
+    margin-bottom: 15px;
+    border-radius: 5px;
+    display: inline-block;
+}
+  </style>
+
+</head>
+
+<body>
+  <div class="color-band"></div>
+  <div id="wrapper">
+    <header class="header">
+      <h1>Dreaming of a Prophetic Life</h1>
+      <h3>Between Romance and Fitrah</h3>
+    </header>
+
+    <div class="main-content">
+      
+    <p class="pages date">July 2023</p>
+    <img class="qualifications-img" src="../../media/imgs/fit-at-50-one-year-update.png" alt="YouTube thumbnail for the video: Fifty and Fit: Year One Update | Lessons & Transformations">
+    <p class="pages">Perhaps it’s all just romance and nostalgia or perhaps it’s the fiṭrah crying out to free itself of this modern contagion we call civilization.</p>
+
+    <div class="embed-responsive embed-responsive-16by9">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/YOXkcz8j3Gc" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <p class="pages date">Date: month and year</p>
+    <p class="pages"><img class="qualifications-img" src="../../media/imgs/fit-at-50-one-year-update.png" alt="YouTube thumbnail for the video: Fifty and Fit: Year One Update | Lessons & Transformations"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <?php /* youtube embed
+    <div class="embed-responsive embed-responsive-16by9">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/Nl8g6a6tST4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    */ ?>
+
+    <?php include('../../footer-home.php'); ?>
+
+  </div>
+
+  <script>
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-62075617-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</body>
+
+</html>
