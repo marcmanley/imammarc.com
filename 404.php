6fa313b47555d8d41b408fabb59b1e4af9146f4c
Marc Manley <marcmanleydev@gmail.com>
2023-07-27 20:09:24 -0700

    new article on Islamic psychology

---
+++ 404.php
@@ -0,0 +1,52 @@
+<?php include('header-main.php'); ?>
+
+<?php include('meta-1.php'); ?>
+
+<?php // begin fit-at-50.php ?>
+
+<?php // Social Media Thumbnail ?> 
+<meta property="og:title" content="Imam Marc Manley">
+	<meta property="og:description" content="Imam Marc Manley's Official Website">
+	<meta property="og:image" content="https://imammarc.com/media/imgs/404-yoda.png">
+	<meta property="og:url" content="https://imammarc.com/404.php"><!-- update thumbnail -->
+	<meta name="twitter:card" content="summary_large_image">
+  <meta property="og:type" content="website">
+
+  <?php include('meta-2.php'); ?>
+
+  <title>Imam Marc &mdash; 404 Error: Page Not Found</title>
+
+</head>
+
+<body>
+  <div class="color-band"></div>
+  <div id="wrapper">
+    <header class="header">
+      <h1>404 Error!</h1>
+      <h3>Page Not Found</h3>
+    </header>
+
+    <div class="main-content">
+    
+    <img class="article-content-img" src="media/imgs/404-yoda.png" alt="Yoda making his famous quote about 'Found someone you have, hmm?'">
+    <p class="pages">Well look at what we got here. Seems you've wondered a bit off the path? Not to worry, <a href="https://imammarc.com">just head back to the home page</a> and I'm sure you'll find what you're looking for.</p>
+
+    <?php include('footer-universal.php'); ?>
+
+  </div>
+
+  <script>
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-62075617-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</body>
+
+</html>
---
+++ pages/articles/islamic-psychology-1.php
@@ -0,0 +1,61 @@
+<?php include('../../header-main.php'); ?>
+
+<?php include('../../meta-1.php'); ?>
+
+<?php // begin fit-at-50.php ?>
+
+<?php // Social Media Thumbnail ?> 
+<meta property="og:title" content="Imam Marc Manley">
+<meta property="og:description" content="Imam Marc Manley - Does Islamic Psychology Live Up to Its Name?">
+<meta property="og:image" content="https://imammarc.com/media/imgs/islamic-psychology-1.png">
+<meta property="og:url" content="https://imammarc.com/pages/articles/islamic-psychology-1.php
+  "><!-- update thumbnail -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="og:type" content="website">
+
+<?php include('../../meta-2.php'); ?>
+
+<title>Imam Marc &mdash; Does Islamic Psychology Live Up to Its Name?</title>
+
+</head>
+
+<body>
+  <div class="color-band"></div>
+  <div id="wrapper">
+    <header class="header">
+      <h1>Islamic Psychology</h1>
+      <h3>Does It Live Up to Its Name?</h3>
+    </header>
+
+    <div class="main-content">
+      <article>
+        <p class="pages date">July 2023</p>
+      <img class="article-content-img" src="../../media/imgs/islamic-psychology-1.png" alt="Picture of Granville Stanley Hall, one of the founding fathers of modern psychology">
+      <blockquote class="clementine-quote drop-cap">“Cultural sociologist Eva Illouz (2008) makes the argument that a therapeutic ethos now transcends all national borders and regional differences, hereby providing the basis for a global psychological mentality and discourse about individuality. She claims that no other cultural framework, with the exception of political liberalism and market liberalism's economic logic of profit, has exercised as much influence on the twentieth century's ideas about the individual. The Danish psychologist Svend Brinkmann (2008) has stated that modern psychology has become so predominant in our understanding of ourselves and the surrounding world that it constitutes what the philosopher Charles Taylor (2004) has called a social imaginary, which is not only a specific theory espoused by many about the nature of the world, but the very framework comprising our theories about ourselves and the world.” &mdash; taken from <strong>Ole Jacob Madsen's</strong>, <em>The Therapeutic Turn: How Psychology Altered Western Culture</em>, Routledge (2014)</blockquote>
+      <p class="pages">Increasingly our society moves towards the psychological and away from the theological. This is no less true amongst Muslims in the West who’ve striven to develop an “Islāmic psychology” but I ask this with all good intentions: how are Muslims psychologists going to reconcile the ever-increasing tendency to view human experience exclusively through the lens of the psychological, an ideology rooted, if not in the denial of the soul (though that too is on the rise), certainly an agnosticism towards its existence, and Islām, whose primary sources speak of a human condition dominated by “impurity” and a darkness of the human heart. Personally, and I know many Muslims striving in this field &mdash; some even dear friends, I have not sufficiently seen how Muslims are tackling this divergent view of the human condition in any satisfactory way.</p>
+      <p class="pages">To me, this is why modern secular psychology remains not only largely ineffective in addressing the human condition, not merely from a diagnostic point of view, but simply, as a Muslim, modern psychology does not understand &mdash; from a point of departure &mdash; what a human being is. And while we may debate this essence as religious/non-religious, I cannot however place much trust in a so-called Islāmic psychology (at least as of writing this article) any more than I can a secular one.</p>
+      <blockquote class="clementine-quote">"If the dominant character type of the twentieth century is really what [Philip] Rieff calls 'psychological man,' the consequences for western society are quite incalculable." &mdash; <strong>Gordon Wright and Arthur Mejia, Jr. (ed.)</strong>, <em>An Age of Controversy: Discussion Problems in Twentieth-Century European History</em> (1963). Quoted in <strong>Philip Rieff's</strong> <em>The Triumph of the Therapeutic: Uses of Faith After Freud</em> (1966)</blockquote>
+      <p class="pages">I leave you with the following quote from one of the founding fathers of modern psychology, Granville Stanley Hall [1844—1924], to ponder while I ask one last question: <em>"Did Islamic psychology lose its soul along the way?"</em></p>
+      <blockquote class="clementine-quote">“All the great problems of our age are becoming more and more psychological the better we understand them. The world needs a new psychology larger in all its dimensions more than it needs anything else.” &mdash; <strong>Madsen</strong>, <em>The Therapeutic Turn</em></blockquote>
+
+      </article>
+
+    <?php include('../../footer-home.php'); ?>
+
+  </div>
+
+  <script>
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-62075617-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</body>
+
+</html>
