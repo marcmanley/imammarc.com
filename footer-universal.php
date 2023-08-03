e174dcbe38390817a0cc4cbeb799180105cbae13
Marc Manley <marcmanleydev@gmail.com>
2023-08-02 21:17:46 -0700

    updating footer

--- footer-universal.php
+++ footer-universal.php
@@ -1,5 +1,5 @@
 
 <?php // begin footer-universal.php ?>
 <footer id="main-footer">
-	<p>&copy; 2023 Marc Manley &#8226; ARR &#8226; Valid HTML5</p>
+<p>&copy; 2023 Marc Manley<!-- &#8226; ARR &#8226; Valid HTML5--></p>
 </footer>
