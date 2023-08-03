c81170140cbd071d7152e1af74bec4a4c00101bd
Marc Manley <marcmanleydev@gmail.com>
2023-08-02 21:16:10 -0700

    adding min-width: 25ch to the .arabic-normal class for mobile

--- footer-home.php
+++ footer-home.php
@@ -7,5 +7,5 @@
   </div>
 
 <footer id="main-footer">
-	<p>&copy; 2023 Marc Manley &#8226; ARR &#8226; Valid HTML5</p>
+	<p>&copy; 2023 Marc Manley<!-- &#8226; ARR &#8226; Valid HTML5--></p>
 </footer>
