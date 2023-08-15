fb59ad5807da51eddf3951a70ac512e12cd7dba4
Marc Manley <marcmanleydev@gmail.com>
2023-08-14 18:36:48 -0700

    new Arabic bio

---
+++ pages/non-articles/bio-arabic.php
@@ -0,0 +1,54 @@
+<?php include('../../header-main.php'); ?>
+
+<?php include('../../meta-1.php'); ?>
+
+<?php // begin fit-at-50.php ?>
+
+<?php // Social Media Thumbnail ?> 
+<meta property="og:title" content="Imam Marc Manley">
+	<meta property="og:description" content="Imam Marc Manley's Official Website">
+	<meta property="og:image" content="https://imammarc.com/media/imgs/fit-at-50-one-year-update.png">
+	<meta property="og:url" content="https://imammarc.com/pages/non-articles/fit-at-50.php"><!-- update thumbnail -->
+	<meta name="twitter:card" content="summary_large_image">
+  <meta property="og:type" content="website">
+
+  <?php include('../../meta-2.php'); ?>
+
+  <title>الإمام مارك مانلي (أبو زيان المرواني) &mdash; نسبه وشأنه</title>
+
+</head>
+
+<body>
+  <div class="color-band"></div>
+  <div id="wrapper">
+    <header class="header arabic-normal">
+      <h1>مارك مانلي</h1>
+      <h3 style="font-style: normal">إمام وخطيب بأبلند كالفرنيا</h3>
+    </header>
+
+    <div class="main-content arabic-normal">
+      
+    <p class="pages date">محرم ١٤٤٥</p>
+    <img class="article-content-img" src="../../media/imgs/arabic-bio.png" alt="الإمام مارك مانلي">
+    <p class="pages arabic-normal" style="font-size: 1.8rem">الإمام مارك مانلي (أبو زيان المرواني) ولد في شهر محرم في عام ألف وتسعمائة والثالث وسبعين هجري ((ألف وثلاثمائة والثالث وتسعين ميلادي))  في المدينة ديترويت بولاية مشيغان. كان مسيحيا مولدا وبفضل ربه هو أسلم في عام شهر محرم ألف أربعمائة واثنا عشر هجريا وكان عمره ثمانية عشر عاما. بعد دخوله في الإسلام بدأ دراسته في العلوم الإسلامية المختلفة مع عدد مشايخ في منطقة ديترويت وديربورن. بدأ مسيرته التعليمية كمدرس للغة العربية في المدرسة الإسلامية خلال نهاية الأسبوع بآن أربور ميشيغان. ثم انتقل ليعمل بجامعة بنسلفانيا كقسيس للمسلمين. وبعد فترة قصيرة، تم تعيينه كإمام في المسجد ICIE برانشو كوكامونجا. وبعد عام واحد من ذلك، ساهم في تأسيس المصلى "<a href="http://muslim.center">Middle Ground</a>" وهو الآن إمامه المتفرغ.</p>
+    <p class="pages arabic-normal" style="font-size: 1.8rem">الإمام مارك هو أمريكي وطنيا وإفريقي أمريكي شعبيا وفي إضافة ذلك هو أندلسي جزئي نسبا من طريق أسرة والدته الكاربية. الآن هو يشغل بالإمامة بمدينة أبلند في جنوب كالفرنيا.</p>
+
+    <?php include('../../footer-home.php'); ?>
+
+  </div>
+
+  <script>
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-62075617-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</body>
+
+</html>
